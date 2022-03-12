1e723766fdb1fac65934010506154994d5d516ad
mebukibu <masa1280g@gmail.com>
2022-03-12 20:47:07 +0900

    registerを追加

---
+++ cpu/src/register.v
@@ -0,0 +1,13 @@
+module register (clk, rst_n, load, d, q);
+  parameter N = 64;
+
+  input clk, rst_n, load;
+  input [N-1:0] d;
+  output [N-1:0] q;
+  reg [N-1:0] q;
+
+  always @(posedge clk, negedge rst_n)
+    if (!rst_n) q <= 0;
+    else if(load) q <= d;
+  
+endmodule
