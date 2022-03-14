a8be4e5e2171933b562cee514af5b61aaf3adc02
mebukibu <masa1280g@gmail.com>
2022-03-14 20:34:20 +0900

    フラグレジスタをロードできるように修正

--- cpu/src/flag.v
+++ cpu/src/flag.v
@@ -1,13 +1,13 @@
-module flag (clk, rst_n, d, q);
+module flag (clk, rst_n, load, d, q);
 
-  input clk, rst_n;
+  input clk, rst_n, load;
   input [63:0] d;
   output [1:0] q;
   reg [1:0] q;
 
   always @(posedge clk, negedge rst_n)
     if (!rst_n) q <= 0;
-    else begin
+    else if (load) begin
       q[0] <= (d == 0);
       q[1] <= d[63];
     end
